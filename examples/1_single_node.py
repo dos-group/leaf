50ff450692705643b808cb83f9dbaa9dcf48a560
Philipp Wiesner <wiesner@tu-berlin.de>
2022-05-16 15:50:55 +0100

    release 0.4.0
    - Nodes can now have a leaf.mobility.Location
    - Refactored PowerModel into self-contained class
    - Adapted an cleaned up some experiments

--- examples/1_single_node.py
+++ examples/1_single_node.py
@@ -3,7 +3,7 @@ import simpy
 
 from leaf.application import Task
 from leaf.infrastructure import Node
-from leaf.power import PowerModelNode, PowerMeasurement, power_meter
+from leaf.power import PowerModelNode, PowerMeasurement, PowerMeter
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s\t%(message)s')
@@ -13,34 +13,33 @@ def main():
     """Simple example that adds and removes a task from a single node
 
     Log Output:
-        DEBUG	0: PowerMeter1: PowerMeasurement(dynamic=0.0W, static=10W)
-        DEBUG	1: PowerMeter1: PowerMeasurement(dynamic=0.0W, static=10W)
-        DEBUG	2: PowerMeter1: PowerMeasurement(dynamic=0.0W, static=10W)
+        DEBUG	0: PowerMeter1: PowerMeasurement(dynamic=0.00W, static=10.00W)
+        DEBUG	1: PowerMeter1: PowerMeasurement(dynamic=0.00W, static=10.00W)
+        DEBUG	2: PowerMeter1: PowerMeasurement(dynamic=0.00W, static=10.00W)
         INFO	task has been added at 3
-        DEBUG	3: PowerMeter1: PowerMeasurement(dynamic=20.0W, static=10W)
-        DEBUG	4: PowerMeter1: PowerMeasurement(dynamic=20.0W, static=10W)
-        DEBUG	5: PowerMeter1: PowerMeasurement(dynamic=20.0W, static=10W)
-        DEBUG	6: PowerMeter1: PowerMeasurement(dynamic=20.0W, static=10W)
-        DEBUG	7: PowerMeter1: PowerMeasurement(dynamic=20.0W, static=10W)
+        DEBUG	3: PowerMeter1: PowerMeasurement(dynamic=20.00W, static=10.00W)
+        DEBUG	4: PowerMeter1: PowerMeasurement(dynamic=20.00W, static=10.00W)
+        DEBUG	5: PowerMeter1: PowerMeasurement(dynamic=20.00W, static=10.00W)
+        DEBUG	6: PowerMeter1: PowerMeasurement(dynamic=20.00W, static=10.00W)
+        DEBUG	7: PowerMeter1: PowerMeasurement(dynamic=20.00W, static=10.00W)
         INFO	task has been removed at 8
-        DEBUG	8: PowerMeter1: PowerMeasurement(dynamic=0.0W, static=10W)
-        DEBUG	9: PowerMeter1: PowerMeasurement(dynamic=0.0W, static=10W)
+        DEBUG	8: PowerMeter1: PowerMeasurement(dynamic=0.00W, static=10.00W)
+        DEBUG	9: PowerMeter1: PowerMeasurement(dynamic=0.00W, static=10.00W)
         INFO	Total power usage: 200.0 Ws
     """
     # Initializing infrastructure and workload
     node = Node("node1", cu=100, power_model=PowerModelNode(max_power=30, static_power=10))
     task = Task(cu=100)
 
-    measurements = []
+    power_meter = PowerMeter(node, name="PowerMeter1")
 
     env = simpy.Environment()  # creating SimPy simulation environment
     env.process(placement(env, node, task))  # registering workload placement process
-    env.process(power_meter(env, node, name="PowerMeter1",
-                            callback=lambda m: measurements.append(m)))  # registering power metering process
+    env.process(power_meter.run(env))  # registering power metering process
 
     env.run(until=10)  # run simulation for 10 seconds
 
-    logger.info(f"Total power usage: {float(PowerMeasurement.sum(measurements))} Ws")
+    logger.info(f"Total power usage: {float(PowerMeasurement.sum(power_meter.measurements))} Ws")
 
 
 def placement(env, node, task):
