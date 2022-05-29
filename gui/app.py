0bcf857f0c2b8df862923ea8f7a8d71f76bf27c7
yanka <kernerya@gmail.com>
2022-05-29 12:41:06 +0200

    non selectable nodes bug fixed

--- gui/app.py
+++ gui/app.py
@@ -21,11 +21,11 @@ bar_unselected_opacity = 0.8
 
 
 PLOT_CONTAINER = {
-     "borderRadius": "10px",
-     "background": "white",
-     "padding": "3px"
+    "borderRadius": "10px",
+    "background": "white",
+    "padding": "3px",
 
- }
+}
 
 NODE_INFO = {
     "borderRadius": "10px",
@@ -84,11 +84,11 @@ SELECT_BUTTON = {
     "color": "#A2C2C2",
 }
 
-DESELECT_BUTTON = {
+GRAY_BUTTON = {
     "padding": "15px",
     "width": "150px",
     "margin": "10px",
-    "backgroundColor": "black",
+    "backgroundColor": "gray",
     "position": "absolute",
     "zIndex": "10",
     "top": "40px",
@@ -99,12 +99,29 @@ DESELECT_BUTTON = {
     "color": "#A2C2C2",
 
 }
+DESELECT_BUTTON = {
+    "display":"none",
+    "padding": "15px",
+    "width": "150px",
+    "margin": "10px",
+    "backgroundColor": "white",
+    "position": "absolute",
+    "zIndex": "10",
+    "top": "40px",
+    "left": "170px",
+    "borderRadius": "50px",
+    "fontSize": "larger",
+    "border": "3px #A2C2C2 solid",
+    "color": "#A2C2C2",
+
+}
+
 
 
 template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
 
 #def randomizeColor():
-    #return random.choice(colors)
+#return random.choice(colors)
 
 
 
@@ -142,19 +159,19 @@ def infrastructure_to_cyto_dict(infrastructure):
     return elements
 
 
-# def blank_fig(height):
-#     """
-#     Build blank figure with the requested height
-#     """
-#     return {
-#         "data": [],
-#         "layout": {
-#             "height": height,
-#             "template": template,
-#             "xaxis": {"visible": False},
-#             "yaxis": {"visible": False},
-#         },
-#     }
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
 
 
 def power_fig(node_measurements, node_ids: List[str]):
@@ -218,7 +235,13 @@ def highlight_selectable_nodes():
 
 
 config, infrastructure, node_measurements, link_measurements = load_data("../examples/smart_city_traffic/vis_results/fog_2")
+timeseries_chart = dcc.Graph(
+    config={"displayModeBar": True},
+    style={
+        "borderRadius": "20px",
 
+    },
+)
 
 def main():
     # https://dash.plotly.com/cytoscape/reference
@@ -240,15 +263,6 @@ def main():
 
     )
 
-    timeseries_chart = dcc.Graph(
-        #figure=blank_fig(500),
-        config={"displayModeBar": True},
-        style={
-            "borderRadius": "20px",
-
-        }
-    )
-
     app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
     node_info = html.Div([
@@ -259,50 +273,61 @@ def main():
     node_panel = html.Div([
 
          dbc.Row(
-             timeseries_chart, className='plotContainer', style=PLOT_CONTAINER
-
-            ), node_info
-         ], style=NODEPANEL_STYLE)
+             timeseries_chart, className='plotContainer', style=PLOT_CONTAINER, id="test"
+        
+         ),
+         node_info
+    ], style=NODEPANEL_STYLE, id="nodepanelID")
 
     app.layout = html.Div([
         node_panel,
         dbc.Row([
             dbc.Col(html.H5(children=[""], className='header')),
-        ], style={"backgroundColor": "rgba(239, 239, 239, 1)"}),
+        ], style={"backgroundColor": "rgba(239, 239, 239, 1)"}, id="headerID"),
         html.Button(children=['Select all'], id="select", className="selectButton", n_clicks=0, style=SELECT_BUTTON),
+        html.Button(children=['Deselect all'], id="deselect", n_clicks=0, style=DESELECT_BUTTON),
 
 
         dbc.Row([
             dbc.Col(html.Div(network, className='networkContainer')),
-        ], className='childrenContainer')
-    ], id="wrapperDiv", style={"overflow": "hidden"})
+        ], className='childrenContainer', id="networkID")
+    ],  style={"overflow": "hidden"}, id="applayoutID")
 
     last_plot: [] = [None]
     @app.callback(
         Output(timeseries_chart, component_property='figure'),
-        Output('select', 'children'),
-        Output('select','style'),
+        Output('select', 'style'),
+        Output('deselect', 'style'),
         Input(network, component_property='selectedNodeData'),  # tapNodeData
         Input('select', 'n_clicks'),
+        #wenn auf deselect geklickt wird
     )
-    def update_plot(input_value, n_clicks):
+    def update_plot(input_value, n_clicks_select):
         print("update plot")
         print(input_value)
-        if input_value is None and n_clicks is not None:
+        SELECT_BUTTON["backgroundColor"] = "white"
+        if (input_value is None or not input_value) and n_clicks_select is not None:
+            if n_clicks_select > 0:
+                print("return A")
+                SELECT_BUTTON["backgroundColor"] = "gray"
+                DESELECT_BUTTON["display"] = "block"
+                return power_fig(node_measurements, get_selectable_nodes()), SELECT_BUTTON, DESELECT_BUTTON
+        DESELECT_BUTTON["display"] = "none"
 
-            if n_clicks > 0:
-                return power_fig(node_measurements, get_selectable_nodes()), ['Deselect all'], DESELECT_BUTTON
         if input_value is None:
             print("nothing selected")
-            return {}, ["Select all"], SELECT_BUTTON
+            return power_fig(node_measurements, []), SELECT_BUTTON, DESELECT_BUTTON
         else:
             print("graf")
             if not input_value:
                 print("dont update")
-                return power_fig(node_measurements, last_plot[0]), ['Deselect all'],DESELECT_BUTTON
+                return power_fig(node_measurements, last_plot[0]), SELECT_BUTTON, DESELECT_BUTTON
             else:
-                last_plot[0] = [el["id"] for el in input_value]
-                return power_fig(node_measurements, last_plot[0]),  ['Deselect all'],DESELECT_BUTTON
+                if input_value[0]["id"] in get_selectable_nodes():
+                    last_plot[0] = [el["id"] for el in input_value]
+                    return power_fig(node_measurements, last_plot[0]), SELECT_BUTTON, DESELECT_BUTTON
+                else:
+                    return power_fig(node_measurements, []), SELECT_BUTTON, DESELECT_BUTTON
 
     def is_node_selectable(selectednodedata):
         if selectednodedata[0]["id"] in get_selectable_nodes():
@@ -349,15 +374,15 @@ def main():
                     el["style"]["height"] = '60px'
 
         if is_first_call:
-                NETWORK_STYLESHEET.append({
-                    "selector": "." + nodeID,
-                    "style": {
-                        "backgroundColor": "#A2C2C2",
-                        'content': 'data(label)',
-                        'width': "60px",
-                        'height': "60px",
-                    }
-                })
+            NETWORK_STYLESHEET.append({
+                "selector": "." + nodeID,
+                "style": {
+                    "backgroundColor": "#A2C2C2",
+                    'content': 'data(label)',
+                    'width': "60px",
+                    'height': "60px",
+                }
+            })
 
     last_selected_node_content: [] = [None]
     n_clicks_backup = [0]
@@ -373,16 +398,8 @@ def main():
         print(n_clicks)
         print(n_clicks_backup[0])
 
-        # n_clicks_backup[0] += 1
-        # if n_clicks_backup[0] == n_clicks:
-        #     print("clicked")
-        # else:
-        #     n_clicks_backup[0] -= 1
-        #     print("not clicked button")
-        # print(n_clicks_backup[0])
-
         #nichts wurde selektiert aber der button wurde geklickt
-        if selectedNodeData is None and n_clicks is not None:
+        if (selectedNodeData is None or not selectedNodeData) and n_clicks is not None:
 
             close_node_panel()
             print("n_clicks update nodepanel")
@@ -401,40 +418,41 @@ def main():
                 #
                 return NODEPANEL_STYLE, [x, dbc.Col(timeseries_chart, style=PLOT_CONTAINER), node_info], NETWORK_STYLESHEET
             else:
-                if is_node_panel_open() and not selectedNodeData and n_clicks is None:
-                    close_node_panel()
+                return NODEPANEL_STYLE, [last_selected_node_content[0], dbc.Col(timeseries_chart, style=PLOT_CONTAINER),  node_info], NETWORK_STYLESHEET
+
         #button wurde nicht geklickt
         else:
             if is_node_panel_open() and not selectedNodeData and n_clicks is None:
                 close_node_panel()
                 print("button not clicked")
 
-        reset_dash_nodes()
-        if selectedNodeData is None:
-            #selected is None
-            close_node_panel()
-            return NODEPANEL_STYLE, "", NETWORK_STYLESHEET
-        else:
-            # if nothing selected or node is selectable
-            if not selectedNodeData or is_node_selectable(selectedNodeData):
-                # if nodePanel is open and nothing selected
-                if is_node_panel_open() and not selectedNodeData:
-                    close_node_panel()
-                    print(selectedNodeData)
-
-                    #wenn man zu macht verschwindet der graf
-                    return NODEPANEL_STYLE, [last_selected_node_content[0], dbc.Col(timeseries_chart, style=PLOT_CONTAINER),  node_info], NETWORK_STYLESHEET
-
-                # nodePanel is closed and needs to be open
-                else:
-                    last_selected_node_content[0] = ', '.join([el["id"] for el in selectedNodeData])
-
-                    set_selected_nodes([el["id"] for el in selectedNodeData])
-                    open_node_panel()
-                    return NODEPANEL_STYLE, [ last_selected_node_content[0], dbc.Col(timeseries_chart, style=PLOT_CONTAINER) , node_info], NETWORK_STYLESHEET
+            reset_dash_nodes()
+            if selectedNodeData is None:
+                #selected is None
+                close_node_panel()
+                return NODEPANEL_STYLE, "", NETWORK_STYLESHEET
             else:
-                # node is not selectable
-                print("node is not selectable")
+                # if nothing selected or node is selectable
+                if not selectedNodeData or is_node_selectable(selectedNodeData):
+                    # if nodePanel is open and nothing selected
+                    if is_node_panel_open() and not selectedNodeData:
+                        close_node_panel()
+                        print(selectedNodeData)
+
+                        #wenn man zu macht verschwindet der graf
+                        return NODEPANEL_STYLE, [last_selected_node_content[0], dbc.Col(timeseries_chart, style=PLOT_CONTAINER),  node_info], NETWORK_STYLESHEET
+
+                    # nodePanel is closed and needs to be open
+                    else:
+                        last_selected_node_content[0] = ', '.join([el["id"] for el in selectedNodeData])
+
+                        set_selected_nodes([el["id"] for el in selectedNodeData])
+                        open_node_panel()
+                        return NODEPANEL_STYLE, [ last_selected_node_content[0], dbc.Col(timeseries_chart, style=PLOT_CONTAINER) , node_info], NETWORK_STYLESHEET
+                else:
+                    # node is not selectable
+                    print("node is not selectable")
+                    return NODEPANEL_STYLE, [ last_selected_node_content[0], dbc.Col(timeseries_chart, style=PLOT_CONTAINER) , node_info], NETWORK_STYLESHEET
 
     app.run_server(debug=True)
 
